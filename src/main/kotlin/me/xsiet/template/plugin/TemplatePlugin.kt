0af4ddca182c78e9a8be2582ccb0ba65f5b408ee
xsiet <jaemin0299@gmail.com>
2024-03-02 17:03:46 +0900

    Update TemplatePlugin.kt

--- src/main/kotlin/me/xsiet/template/plugin/TemplatePlugin.kt
+++ src/main/kotlin/me/xsiet/template/plugin/TemplatePlugin.kt
@@ -4,7 +4,7 @@ import dev.jorel.commandapi.CommandAPI
 import dev.jorel.commandapi.CommandAPIBukkitConfig
 import org.bukkit.plugin.java.JavaPlugin
 
-internal class TemplatePlugin : JavaPlugin() {
+class TemplatePlugin: JavaPlugin() {
     override fun onLoad() {
         CommandAPI.onLoad(CommandAPIBukkitConfig(this))
     }
